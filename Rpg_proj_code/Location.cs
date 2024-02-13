acf0846dfca7281cc2a4f27d9596334dc3c6db0c
ItsKoiya <1041901@hr.nl>
2024-02-13 13:29:19 +0100

    Location.cs toegevoegd.
    Na mededeling van Alex Location.cs aangemaakt en toegevoegd zoals beschreven in zijn bericht.
    
    https://teams.microsoft.com/l/message/19:SK7g9kXoYUZvcj-3pP1Zk-Iz0X7GzlaLVml5rehhklU1@thread.tacv2/1707827067391?tenantId=ca6fbace-7cba-4d53-8681-a06284f7ff46&groupId=532116ba-84e4-4378-964a-5e88f71a2d10&parentMessageId=1707827067391&teamName=CMI-INF1C%202324%20OP34%20OODP%2FProject%20B%2FSLC&channelName=General&createdTime=1707827067391

---
+++ Rpg_proj_code/Location.cs
@@ -0,0 +1,11 @@
+public class Location {
+    public int ID;
+    public string Name;
+    public string Description;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationtoNorth;
+    public Location LocationtoEast;
+    public Location LocationtoSouth;
+    public Location LocationtoWest;
+}
