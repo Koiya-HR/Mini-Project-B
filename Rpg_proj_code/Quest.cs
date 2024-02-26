b3a41611873987ef9f72974bf9b25a834dd355d2
TurboGreaser <creamsupreme666@gmail.com>
2024-02-26 14:21:47 +0100

    quest reward user story done

--- Rpg_proj_code/Quest.cs
+++ Rpg_proj_code/Quest.cs
@@ -8,11 +8,30 @@ public class Quest
 
     public Item Reward;
 
+    public bool started = false;
+    public bool finished = false;
+
     public Quest(int id, string name, string description, Item reward)
     {
         Description = description;
         ID = id;
         Name = name;
         Reward = reward;
+    }
+
+    public void start_quest()
+    {
+        started = true;
+        Console.WriteLine(Description);
+    }
+
+    public Item finish_quest()
+    {
+        // returns reward
+        finished = true;
+        Console.WriteLine($"{Name}: Completed!");
+        Console.WriteLine($"Obtained a {Reward}");
+        return Reward;
+
     }
 }
